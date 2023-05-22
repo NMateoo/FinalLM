3e7934f58487d50569606e8bf26c033d078a5f28
Nicolás Mateo <55945912+NMateoo@users.noreply.github.com>
2023-05-22 17:42:10 +0200

    1

--- ajustes.js
+++ ajustes.js
@@ -2,22 +2,39 @@
 
 function aplicarCambios() {
     // mostramos en consola los valores recogidos en la pagina
-    var colorFondoDia = document.getElementsByName('colorElegido')[0];
-    console.log('Color fondo dia: ' + colorFondoDia.value);
-    var colorLetraDia = document.getElementsByName('colorElegido')[1];
-    console.log('Color letra dia: ' + colorLetraDia.value);
+    var colorFondoDia1 = document.getElementsByName('colorElegido')[0];
+    console.log('Color fondo dia 1: ' + colorFondoDia1.value);
+    var colorFondoDia2 = document.getElementsByName('colorElegido')[1];
+    console.log('Color fondo dia 2: ' + colorFondoDia2.value2);
+    var colorLetraDia1 = document.getElementsByName('colorElegido')[2];
+    console.log('Color letra dia: ' + colorLetraDia1.value);
+    var colorLetraDia2 = document.getElementsByName('colorElegido')[3];
+    console.log('Color letra dia: ' + colorLetraDia2.value);
 
-    var colorFondoNoche = document.getElementsByName('colorElegido')[2];
-    console.log('Color fondo noche: ' + colorFondoNoche.value);
-    var colorLetraNoche = document.getElementsByName('colorElegido')[3];
-    console.log('Color letra noche: ' + colorLetraNoche.value);
+    // aplicamos el color a todo el documento
+    document.documentElement.style.setProperty("--colorFondo1", colorFondoDia1.value);
+    document.documentElement.style.setProperty("--colorFondo", colorFondoDia2.value);
+    document.documentElement.style.setProperty("--colorLetrasN", colorLetraDia1.value);
+    document.documentElement.style.setProperty("--colorLetrasG", colorLetraDia2.value);
 
-    var tam = document.getElementsByName('tamFuente')[0];
-    var tamelegido = tam[tam.selectedIndex].value;
-
-    // aplicamos el color y el tamaño a todo el documento
-    document.documentElement.style.fontSize = parseFloat(tamelegido) + "rem";
-    document.documentElement.style.setProperty("--colorFondo", colorFondoDia.value);
-    //document.documentElement.style.setProperty("--colorFondo", colorLetraDia.value);
-
+    window.localStorage.setItem('colorFondo1', colorFondoDia1.value);
+    window.localStorage.setItem('colorFondo2', colorFondoDia2.value);
+    window.localStorage.setItem('colorLetra1', colorLetraDia1.value);
+    window.localStorage.setItem('colorLetra2', colorLetraDia2.value);
 }
+
+// Leer los colores guardados en localStorage
+function leerColoresGuardados() {
+    var colorFondo1 = window.localStorage.getItem("colorFondo1");
+    var colorFondo2 = window.localStorage.getItem("colorFondo2");
+    var colorLetras1 = window.localStorage.getItem("colorLetras1");
+    var colorLetras2 = window.localStorage.getItem("colorLetras2");
+
+    if (colorFondo1 && colorFondo2 && colorLetras1 && colorLetras2) {
+        document.documentElement.style.setProperty("--colorFondo1", colorFondo1);
+        document.documentElement.style.setProperty("--colorFondo", colorFondo2);
+        document.documentElement.style.setProperty("--colorLetrasN", colorLetras1);
+        document.documentElement.style.setProperty("--colorLetrasG", colorLetras2);
+    }
+}
+window.addEventListener('load', leerColoresGuardados);
--- diaNoche.js
+++ diaNoche.js
@@ -18,22 +18,11 @@ if (modo == "dia") {
 }
 
 function ponerModoDia() {
-    let todo = document.getElementById("todo");
-    let inicio = document.getElementById("inicio");
-    let hero = document.getElementsByClassName("hero-image");
-    let artistas = document.getElementById("artistas");
-    let horario = document.getElementById("horario");
-    let entradas = document.getElementById("entradas");
-    let noticias = document.getElementById("noticias");
-    let localizacion = document.getElementById("localizacion");
-    let contacto = document.getElementById("contacto");
+    document.documentElement.style.setProperty('--colorFondo1','#f1f1f1')
+    document.documentElement.style.setProperty('--colorLetrasN','#000')
+    document.documentElement.style.setProperty('--colorFondo','#ffffff')
+    document.documentElement.style.setProperty('--colorLetrasG','#888888')
 
-    todo.style.color="black";
-    todo.style.backgroundColor="white";
-    inicio.style.color="black";
-    inicio.style.backgroundColor="#f1f1f1";
-    contacto.style.color="#242323";
-    contacto.style.backgroundColor="#f1f1f1";
     window.localStorage.setItem("modoGuardado", "dia");
     let iconoDia = document.getElementById("dia");
     iconoDia.style.display = "none";
@@ -42,26 +31,11 @@ function ponerModoDia() {
 }
 
 function ponerModoNoche() {
-    let todo = document.getElementById("todo");
-    let inicio = document.getElementById("inicio");
-    let hero = document.getElementsByClassName('hero-image');
-    let artistas = document.getElementById("artistas");
-    let horario = document.getElementById("horario");
-    let entradas = document.getElementById("entradas");
-    let noticias = document.getElementById("noticias");
-    let localizacion = document.getElementById("localizacion");
-    let contacto = document.getElementById("contacto");
+    document.documentElement.style.setProperty('--colorFondo1','#222222')
+    document.documentElement.style.setProperty('--colorLetrasN','#ffffff')
+    document.documentElement.style.setProperty('--colorFondo','#333333')
+    document.documentElement.style.setProperty('--colorLetrasG','#aaaaaa')
 
-    todo.style.color="white";
-    todo.style.backgroundColor="black";
-    inicio.style.color="white";
-    inicio.style.backgroundColor="#242323";
-    hero.style.color="white";
-    hero.style.backgroundColor="black";
-    contacto.style.color="white";
-    contacto.style.backgroundColor="#242323";
-    localizacion.style.color="white";
-    localizacion.style.backgroundColor="black";
     window.localStorage.setItem("modoGuardado", "noche")
     let iconoDia = document.getElementById("dia");
     iconoDia.style.display = "block";
